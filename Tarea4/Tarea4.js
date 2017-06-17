18115fb6fac4082b235004dcdd74182af93a5718
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2017-06-16 22:17:20 -0500

    Tarea 4

--- Tarea4/Tarea4.js
+++ Tarea4/Tarea4.js
@@ -1,18 +1,15 @@
 const async = require('async');
-
+//Functions
 function declaration(cb){
 	cb(null, 0, 1);
 }
-
-/*function declaration2(cb){
-	cb(null, 1000, 0);
-*/
-
 function fibonacci3(ant2, ant1, cb){
 	num3 = ant2+ant1;
 	return cb(null, ant1, num3);
 }
-
+function declaration2(cb){
+	cb(null, 1000, 0);
+}
 function hour(time, timess, cb){
 	t = time;
 	setTimeout(function(){
@@ -30,7 +27,7 @@ function hour(time, timess, cb){
 	}, t);
 	t=time+1000;
 }
-
+//Waterfalls
 async.waterfall([
 	declaration,
 	fibonacci3,
@@ -41,14 +38,29 @@ async.waterfall([
 	fibonacci3,
 	fibonacci3,
 	fibonacci3,
-	//declaration2,
-	//hour,
 	], (err, cb) => {
 		if(err){
 			console.log("Error");
 		}
 		else{
 			console.log(num3);
-			console.log('-------------------');
+			console.log('---------------');
+		}
+});
+async.waterfall([
+	declaration2,
+	hour,
+	hour,
+	hour,
+	hour,
+	hour,
+	hour,
+	hour,
+	], (err, cb) => {
+		if(err){
+			console.log("Error");
+		}
+		else{
+			return;
 		}
 });
