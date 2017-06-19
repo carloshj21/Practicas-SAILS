e4c671a5ce73c3ecb1eb2763371c1706927db2b2
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2017-06-18 22:47:13 -0500

    Intento Tarea 2 :(

--- Tarea2/index.js
+++ Tarea2/index.js
@@ -6,70 +6,57 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function Ver(){
+function Ver() {
   var eventos = fs.readFileSync('Eventos.txt').toString().split('\n');
+  var x = 1;
+  console.log('---------------------------\n');
   for (date in eventos)
   {
-    console.log(eventos[date]);
+    console.log(x + ' ' + eventos[date]);
+    x++;
   }
-  /*console.log(eventos);
-  eventos.splice(2,1);
-  console.log(eventos);
-  console.log('\n\n');
+  console.log("\n\n*Estos son todos sus Eventos programados*\n\n");
+}
+function Anadir() {
+  rl.question('\n¿Nombre del Evento?:\n', (nombre) => {
+    console.log("Nooooooo :(");
+  });
+}
+function Eliminar() {
+  var eventos = fs.readFileSync('Eventos.txt').toString().split('\n');
+  var x = 1;
+  console.log('---------------------------\n');
   for (date in eventos)
+  {
+    console.log(x + ' ' + eventos[date]);
+    x++;
+  }
+  console.log("\n*Estos son todos sus Eventos*\n\n");
+  console.log("Teclee el número del evento que desea eliminar:");
+  //eventos.splice(0,1);
+  /*for (date in eventos)
   {
     console.log(eventos[date]);
   }*/
-  console.log("\n*Estos son todos sus Eventos*\n\n");
-  //process.exit(0);
-  /*fs.readFile('Eventos.txt', function(error, content){
-    console.log(content.toString());
-    console.log("\n*Estos son todos sus Eventos*\n\n")
-  });*/
+  console.log("\nEvento eliminado");
 }
-//function Anadir{}
-
-//Ver();
-
 function Inicio() {
   console.log('\n BIENVENIDO A SU AGENDA :D,  ¿Qué desea hacer?: \n 1) Ver eventos \n 2) Añadir evento \n 3) Eliminar evento \n 4) Salir \n');
   rl.on('line', (line) =>{
     if(line === '1'){
-      Ver(/*Inicio()*/);
+      Ver();
     }
     if(line === '2'){
       Anadir();
     }
     if(line === '3'){
-      console.log("Eliminar evento?");
+      Eliminar();
     }
   	if(line === '4'){
   		process.exit(0);
   	}
     rl.pause();
-    //Inicio();
-    //rl.close();
   });
 }
 
 Inicio();
-
-
-var fechas = [];
-/*rl.question('\n¿Nombre del Evento?:\n', (nombre) => {
-  fechas.push(nombre);
-  fechas.push('.........');
-  fechas.push(':c');
-  console.log('\n');
-  for (date in fechas)
-  {
-    console.log(eventos[date]);
-  }
-  console.log('\n');
-  eventos.splice(2,1);
-  for (date in eventos)
-  {
-    console.log(eventos[date]);
-  }
-  process.exit(0);
-});*/
